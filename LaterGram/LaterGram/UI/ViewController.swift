a4f0d8472195d4a948feae4ec65030364b00339b
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-03-13 15:10:21 -0500

    Initial folder organization and project setup.

---
+++ LaterGram/LaterGram/UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaterGram
+//
+//  Created by iMac Pro on 3/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
